7d78c9c80ec1c1a9b8719e376109889f78607b92
Soumya Goyal <somyagoyal739@gmail.com>
2019-09-09 07:44:07 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,31 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = Hash.new
+  color_hash = data[:color]
+  color_hash.each do |key, value|
+    value.each do |name|
+      if new_hash.key?(name) == false
+      new_hash[name] = {:color => [], :gender => [], :lives => []}
+      end
+    new_hash[name][:color].push("#{key}")
+    end
+  end
+  gender_hash = data[:gender]
+  gender_hash.each do |key, value|
+    value.each do |name|
+      if new_hash.key?(name) == false
+        new_hash[name] = {:color => [], :gender => [], :lives => []}
+      end
+      new_hash[name][:gender].push("#{key}")
+    end
+  end
+  lives_hash = data[:lives]
+  lives_hash.each do |key, value|
+    value.each do |name|
+      if new_hash.key?(name) == false
+        new_hash[name] = {:color => [], :gender => [], :lives => []}
+      end
+      new_hash[name][:lives].push("#{key}")
+    end
+  end
+  new_hash
 end
