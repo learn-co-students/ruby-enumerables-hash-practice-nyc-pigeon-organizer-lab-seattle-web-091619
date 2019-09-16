904c8a9daf053f9f786282fe9d8edbdd25193924
Cole Ditzler <colejditzler@gmail.com>
2019-09-16 04:35:58 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  traits_by_name = {}
+
+  data.each do |name_of_attribute, attributes|
+    attributes.each do |value_of_attribute, name_of_pidgeon|
+      name_of_pidgeon.each do |name|
+        traits_by_name[name] ||= {}
+        traits_by_name[name][name_of_attribute] ||= []
+        traits_by_name[name][name_of_attribute].push(value_of_attribute.to_s)
+      end
+    end
+  end
+
+  traits_by_name
 end
